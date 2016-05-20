650b7eb296851872b4b7dd35fbad88866b301bf0
Carlos <carlosbatista108@hotmail.com>
2016-05-20 15:37:06 -0400

    Primer commit

---
+++ practica/src/main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by carlosb108 on 5/20/16.
+ */
+
+import org.jsoup.*;
+public class main {
+
+public static void main(String [] args){
+
+
+    System.out.println("Hello");
+
+
+}
+
+}
