cc146b5fb3bb42390b4f4d53f8c93aa9bee97bd1
Carlos <carlosbatista108@hotmail.com>
2016-05-23 11:10:55 -0400

    Url validator

--- practica/src/main.java
+++ practica/src/main.java
@@ -2,6 +2,7 @@
  * Created by carlosb108 on 5/20/16.
  */
 
+import org.apache.commons.validator.routines.UrlValidator;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,48 +21,58 @@ public static void main(String [] args) throws IOException {
     //leemos el url
     Scanner sc = new Scanner(System.in);
     String url;
-    System.out.print("Introduzca url: ");
+    System.out.print("Introduzca  el url: ");
     url = sc.nextLine();
-   //Nombre de la url   Documento desde un URL
-   Document doc = Jsoup.connect(url).get();
-    //Titulo
-   String titulo = doc.title();
-   System.out.print("\n Titulo: " +titulo);
 
-    //Cantidad de lineas
-    int Cantidadlineas = doc.html().split(System.getProperty("line.separator")).length;
-    System.out.printf("\n Total de lineas: "+ Cantidadlineas);
+    String[] schemes = {"http","https"}; // DEFAULT schemes = "http", "https", "ftp"
+    UrlValidator urlValidator = new UrlValidator(schemes);
+    if (!urlValidator.isValid(url)) {
+       System.out.println("url is invalid");
+    } else {
 
-    //Parrafos
-   Elements parrafos = doc.select("p"); // parrafos
-   System.out.print("\n Cantidad de parrafos: " + parrafos.size());
 
-    //Imagenes
-    Elements imagenes = doc.select("img"); // imagenes
-    System.out.print("\n Cantidad de imagenes: "+ imagenes.size());
+        //Nombre de la url   Documento desde un URL
+        Document doc = Jsoup.connect(url).get();
+        //Titulo
+        String titulo = doc.title();
+        System.out.print("\n Titulo: " + titulo);
 
-    //Formularios
-    Elements Formularios = doc.select("form");
-    System.out.print("\n Cantidad de formularios: " + Formularios.size());
-    //Mostrando el tipo y el nombre
-    ArrayList<Elements> inputs = new ArrayList<Elements>();
+        //Cantidad de lineas
+        int Cantidadlineas = doc.html().split(System.getProperty("line.separator")).length;
+        System.out.printf("\n Total de lineas: " + Cantidadlineas);
 
-    for(Element formulario : Formularios){
-        inputs.add(formulario.getElementsByTag("input"));
-    }
-    int contador=1;
-    for (Elements articulos : inputs ){
+        //Parrafos
+        Elements parrafos = doc.select("p"); // parrafos
+        System.out.print("\n Cantidad de parrafos: " + parrafos.size());
 
-        System.out.println("\nFormulario: # " + contador + ":");
-        for(Element input : articulos ){
-            String tipo = input.attr("type");
-            String nombre = input.attr("name");
-            System.out.println("Tipo: " + tipo + "  Nombre: "+ nombre + "\nCampos :" + input);
+        //Imagenes
+        Elements imagenes = doc.select("img"); // imagenes
+        System.out.print("\n Cantidad de imagenes: " + imagenes.size());
+
+        //Formularios
+        Elements Formularios = doc.select("form");
+        System.out.print("\n Cantidad de formularios: " + Formularios.size());
+        //Mostrando el tipo y el nombre
+
+        ArrayList<Elements> inputs = new ArrayList<Elements>();
+
+        for (Element formulario : Formularios) {
+            inputs.add(formulario.getElementsByTag("input"));
         }
-        contador++;
+        int contador = 1;
+        for (Elements articulos : inputs) {
+
+            System.out.println("\nFormulario: # " + contador + ":");
+            for (Element input : articulos) {
+                String tipo = input.attr("type");
+                String nombre = input.attr("name");
+                System.out.println("Tipo: " + tipo + "  Nombre: " + nombre + "\nCampos :" + input);
+            }
+            contador++;
+
+        }
 
     }
-
 
 
     }
